574b79a11f690e7b384538db4ea5addcffecc061
Shebin John <admin@remedcu.com>
2025-08-04 09:47:28 +0200

    Added new signature encoding scheme (#23)

--- actions/README.md
+++ actions/README.md
@@ -11,9 +11,9 @@ Collection of GitHub Actions used to co-sign transactions.
 - [Check Transaction](./check/)
 
   Action to check and co-sign the specified Safe transaction
- 
+
 - [Relay Transaction](./relay/)
-  
+
   Action to relay a Safe transaction that has been approved
 
 ## Setup
@@ -24,14 +24,26 @@ To setup the GitHub actions for yourself it is recommended to use the [Varangian
 
 The actions are setup as a monorepo. Each action is maintained in a separate folder. The `shared-utils` folder contains all shared logic.
 
-* Install dependencies
+- Install dependencies
 
 ```sh
 npm i
 ```
 
-* Build all packages
+- Build all packages
 
 ```sh
 npm run build:all
 ```
+
+### Cosigner Signature Encoding
+
+There are two modes of signature encoding done, one which contains only the signature and the other which also contains the length of the signature.
+
+#### Default Mode
+
+This is the mode where only signature is present based on the cosigner address derived.
+
+#### 1271 Compatible Mode
+
+This is the mode where the signature and the length, both are appended at the end of the signature of a Safe Tx
--- actions/check/src/main.ts
+++ actions/check/src/main.ts
@@ -9,7 +9,7 @@ const getCoSigner = (coSignerMaterial: string): ethers.Wallet => {
   return wallet
 }
 
-const checkTransaction = async (coSignerMaterial: string, safeTx: ExtendedSafeTransaction): Promise<{ coSignerSignature: string, coSignerAddress: string }> => {
+const checkTransaction = async (coSignerMaterial: string, safeTx: ExtendedSafeTransaction, coSignerSig1271Compatible: boolean): Promise<{ coSignerSignature: string, coSignerAddress: string }> => {
   const wallet = getCoSigner(coSignerMaterial)
 
   const safeTxHash = getSafeTxHash(safeTx)
@@ -23,6 +23,13 @@ const checkTransaction = async (coSignerMaterial: string, safeTx: ExtendedSafeTr
   }
   core.info("Generate co-signer signature")
   const coSignerSignature: string = wallet.signingKey.sign(safeTxHash).serialized
+  if (coSignerSig1271Compatible) {
+    // The signature length is appended as a 32 byte word to the end of the signature. And the signature length in this case is always 65 bytes (0x41).
+    const signatureLength = ethers.zeroPadValue("0x41", 32);
+    console.log({ signatureLength });
+    console.log({ coSignerSignature });
+    return { coSignerSignature: coSignerSignature + signatureLength.slice(2), coSignerAddress: wallet.address }
+  }
   console.log({ coSignerSignature });
   return { coSignerSignature, coSignerAddress: wallet.address }
 }
@@ -30,6 +37,7 @@ const checkTransaction = async (coSignerMaterial: string, safeTx: ExtendedSafeTr
 async function run() {
   try {
     const coSignerMaterial = core.getInput('co-signer-material', { required: true });
+    const coSignerSig1271Compatible = core.getBooleanInput('signature-1271-compatible') ?? false;
     const encodedSafeTx = core.getInput('safe-tx')
     if (!encodedSafeTx) {
       // If there is no transaction to check, then we only return the co-signer address
@@ -38,7 +46,7 @@ async function run() {
       return
     }
     const safeTx: ExtendedSafeTransaction = JSON.parse(encodedSafeTx);
-    const output = await checkTransaction(coSignerMaterial, safeTx)
+    const output = await checkTransaction(coSignerMaterial, safeTx, coSignerSig1271Compatible);
     core.setOutput('co-signer-address', output.coSignerAddress);
     core.setOutput('co-signer-signature', output.coSignerSignature);
     /*
