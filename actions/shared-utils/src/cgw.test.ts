94ecab185066075dfb4622ae80bfa1b5e79cc7d2
Richard Meissner <msc.meissner@gmail.com>
2025-11-04 11:35:48 +0000

    Fake origin for cgw

---
+++ actions/shared-utils/src/cgw.test.ts
@@ -0,0 +1,8 @@
+import { describe, assert, it, expect } from "vitest"; // or '@jest/globals'
+import { loadChainInfo } from "./cgw";
+// --- Tests ---
+describe("getChainInfo", () => {
+  it("should be able to load chain info", async () => {
+    console.log(await loadChainInfo("100"))
+  });
+})
